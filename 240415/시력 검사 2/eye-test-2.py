ea4120c480cb04271a908ec5eb1eecbdf1816196
kimmjen <50638199+kimmjen@users.noreply.github.com>
2024-04-15 23:18:00 +0900

    Title: 시력 검사 2, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240415/시력 검사 2/eye-test-2.py
+++ 240415/시력 검사 2/eye-test-2.py
@@ -1,8 +1,8 @@
 a = float(input())
 
 if a >= 1.0:
-    print('High')
-elif a >= 5.0:
-    print('Middle')
+    print("High")
+elif a >= 0.5:
+    print("Middle")
 else:
-    print('Low')
+    print("Low")
