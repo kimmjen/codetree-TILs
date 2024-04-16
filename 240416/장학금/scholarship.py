4c25857c93809565cd07ec11dfea1636ea7de669
kimmjen <50638199+kimmjen@users.noreply.github.com>
2024-04-16 13:26:49 +0900

    Title: 장학금, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240416/장학금/scholarship.py
+++ 240416/장학금/scholarship.py
@@ -1,8 +1,8 @@
 a, b = map(int, input().split())
 
-if a > 90 and b >= 95:
+if a >= 90 and b >= 95:
     print(100000)
-elif a > 90 and b >= 90:
+elif a >= 90 and b >= 90:
     print(50000)
 else:
     print(0)
