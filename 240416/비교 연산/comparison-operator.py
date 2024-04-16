4aef70c44fa16f1e69b3a3b90bb25afcaa8ba6d2
kimmjen <50638199+kimmjen@users.noreply.github.com>
2024-04-16 09:07:57 +0900

    Title: 비교 연산, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240416/비교 연산/comparison-operator.py
@@ -0,0 +1,20 @@
+a, b = map(int, input().split())
+
+c1 = int(a >= b)
+
+c2 = int(a > b)
+
+c3 = int(b >= a)
+
+c4 = int(b > a)
+
+c5 = int(a == b)
+
+c6 = int(a != b)
+
+print(c1)
+print(c2)
+print(c3)
+print(c4)
+print(c5)
+print(c6)
