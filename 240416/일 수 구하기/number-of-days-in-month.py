c917ee453abae329ac8c58d98219bd912b2a015b
kimmjen <50638199+kimmjen@users.noreply.github.com>
2024-04-16 19:21:50 +0900

    Title: 일 수 구하기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240416/일 수 구하기/number-of-days-in-month.py
+++ 240416/일 수 구하기/number-of-days-in-month.py
@@ -3,8 +3,10 @@ a = int(input())
 if a % 2 == 0:
     if a == 2:
         print(28)
-    print(30)
+    else:
+        print(30)
 else:
     if a == 8:
         print(31)
-    print(31)
+    else:
+        print(31)
