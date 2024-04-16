215344f52be339fb337cf5eea54b929646f8bb26
kimmjen <50638199+kimmjen@users.noreply.github.com>
2024-04-16 19:18:51 +0900

    Title: 윤년인가, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240416/윤년인가/is-leap-year.py
+++ 240416/윤년인가/is-leap-year.py
@@ -1,6 +1,12 @@
 a = int(input())
 
 if a % 4 == 0:
-    print('true')
+    if a % 100 == 0:
+        if a % 400 == 0:
+            print('true')
+        else:
+            print('false')
+    else:
+        print('true')
 else:
     print('false')
