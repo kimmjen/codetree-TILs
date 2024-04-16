e00d89e9857dbad7211b8864f4940ef9c3627a97
kimmjen <50638199+kimmjen@users.noreply.github.com>
2024-04-16 19:26:31 +0900

    Title: 일 수 구하기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240416/일 수 구하기/number-of-days-in-month.py
+++ 240416/일 수 구하기/number-of-days-in-month.py
@@ -1,14 +1,21 @@
 a = int(input())
 
-if a % 2 == 0:
-    if a == 2:
-        print(28)
-    elif a == 8:
-        print(31)
-    else:
-        print(30)
-else:
-    if a == 9:
-        print(30)
-    else:
-        print(31)
+def days_in_month(month):
+    # 각 월에 대한 일 수를 딕셔너리에 저장
+    days = {
+        1: 31,
+        2: 28,
+        3: 31,
+        4: 30,
+        5: 31,
+        6: 30,
+        7: 31,
+        8: 31,
+        9: 30,
+        10: 31,
+        11: 30,
+        12: 31
+    }
+    return days[month]
+
+print(days_in_month(a))
