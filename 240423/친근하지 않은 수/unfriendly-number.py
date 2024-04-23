8542fed31eed7da6da2a47af023064258612b3fe
kimmjen <50638199+kimmjen@users.noreply.github.com>
2024-04-23 12:39:46 +0900

    Title: 친근하지 않은 수, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240423/친근하지 않은 수/unfriendly-number.py
+++ 240423/친근하지 않은 수/unfriendly-number.py
@@ -2,7 +2,7 @@ n = int(input())
 
 count = 0
 for i in (1, n + 1):
-    if (i % 2 != 0 and i % 3 != 0) or (i % 5 != 0):
+    if i % 2 != 0 and i % 3 != 0 and i % 5 != 0:
         count += 1
     
 print(count)
