e0f75eedc7e0195b316531e44182f3187d9d66c3
kimmjen <50638199+kimmjen@users.noreply.github.com>
2024-05-05 20:27:19 +0900

    Title: 공약수의 유무, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240505/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240505/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -1,11 +1,14 @@
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
 def has_common_factor(a, b):
-    for i in range(1920, 2881):
-        if a % i == 0 and b % i == 0:
-            return True
-    return False
+    gcd_result = gcd(a, b)
+    return 1920 % gcd_result == 0 and 2880 % gcd_result == 0
 
 # 입력 받기
 a, b = map(int, input().split())
 
 # 결과 출력
-print(0 if has_common_factor(a, b) else 1)
+print(1 if has_common_factor(a, b) else 0)
