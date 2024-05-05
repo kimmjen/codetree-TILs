e2e09881113a70554227685d4b973a92a47fc754
kimmjen <50638199+kimmjen@users.noreply.github.com>
2024-05-05 21:03:46 +0900

    Title: 별표 출력하기, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240505/별표 출력하기/print-star.py
+++ 240505/별표 출력하기/print-star.py
@@ -4,8 +4,7 @@ n = int(input())
 # 짝수일 경우, 별표 출력
 if n % 2 == 0:
     for i in range(1, n + 1):
-        if i % 2 == 0:
-            print("* " * i)
+        print("* " * i)
 # 홀수일 경우, 별표 출력
 else:
     for i in range(1, n + 1):
